df7b858f80b0b4756e51b3599f43e56a06e0bf78
loiclerat <loic.lerat@gmail.com>
2016-05-29 20:16:42 +0200

    main IHM -> code

--- code/main.cpp
+++ code/main.cpp
@@ -1,26 +1,17 @@
-#include "littreelle.h"
-#include"littentiere.h"
+#include <QApplication>
+#include <QWidget>
+#include <QLineEdit>
+#include <QPushButton>
+#include <QHBoxLayout>
+#include <QVBoxLayout>
+#include"qcomputer.h"
 
-using namespace std;
-int main(){
-    littEntiere l(6);
-    littEntiere m(5);
-    (l+m).affichage();
-    littEntiere n=l;
+int main(int argc, char *argv[]) {
+    QApplication app(argc, argv);
+    QComputer fenetre;
 
-    cout<<"\n****************\n";
-    n=m+l;
-    n.affichage();
-    cout<<"\n****************\n";
-    n.NEG();
-    n.affichage();
-    cout<<"\n****************\n";
-    n=m-l;
-    n.affichage();
-    cout<<"\n";
 
-    littReelle r(8,25);
+    fenetre.show();
+    return app.exec();
 
-    return 0;
 }
-

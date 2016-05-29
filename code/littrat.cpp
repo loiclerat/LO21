39c053b0401e006ab580690bac675f152deae778
THordeq <theo.hordequin@gmail.com>
2016-05-29 19:52:46 +0200

    Littérale rationnelle

---
+++ code/littrat.cpp
@@ -0,0 +1,40 @@
+#include "littrat.h"
+
+
+void littrat::simplifier(){
+    cout<<"Simplification de la littérale rationnelle"<<"\n";
+    if (den==0) return;
+    int a=num, b=den;
+    if (a<0) a=-a; if (b<0) b=-b;
+    while(a!=b){ if (a>b) a=a-b; else b=b-a; }
+    num/=a; den/=a;
+    if (den<0) {
+        den=-den;
+        num=-num;
+    }
+}
+
+littrat& littrat::operator+(littrat& b){
+        littrat* res=new littrat(this->getNum()*b.den+this->getDen()*b.num,this->getDen()*b.den);
+        res->simplifier();
+        return (*res);
+}
+
+
+littrat& littrat::operator-(littrat& b){
+    littrat* res=new littrat(this->getNum()*b.den-this->getDen()*b.num,this->getDen()*b.den);
+    res->simplifier();
+    return (*res);
+}
+
+littrat& littrat::operator*(littrat& b){
+    littrat* res=new littrat(this->getNum()*b.num,this->getDen()*b.den);
+    res->simplifier();
+    return (*res);
+}
+
+littrat& littrat::operator/(littrat& b){
+    littrat* res=new littrat(this->getNum()*b.den,this->getDen()*b.num);
+    res->simplifier();
+    return (*res);
+}

7d11ffbc8528b4e7e71eb5674ec0dc478dd20476
Theo <theo.hordequin@gmail.com>
2016-06-01 20:19:29 +0200

    Ajout operateur numerique et operateur.h

--- code/operateur.h
+++ code/operateur.h
@@ -1,4 +1,14 @@
 #ifndef OPERATEUR_H
 #define OPERATEUR_H
+#include "litterale.h"
+
+class operateur {
+protected:
+    int arite;
+public:
+    operateur(int a):arite(a){};
+    virtual litterale& traitement(litterale& a, litterale& b)=0;
+    //virtual litterale* traitement(litterale* a)=0;
+};
 
 #endif // OPERATEUR_H
--- code/operateur_numerique.h
+++ code/operateur_numerique.h
@@ -1,4 +1,14 @@
 #ifndef OPERATEUR_NUMERIQUE_H
 #define OPERATEUR_NUMERIQUE_H
+#include "operateur.h"
+
+class operateur_numerique : public operateur {
+public:
+    operateur_numerique(int a):operateur(a){};
+    virtual litterale& traitement(litterale& a, litterale& b)=0;
+    //virtual litterale* traitement(litterale* a)=0;
+};
+
+
 
 #endif // OPERATEUR_NUMERIQUE_H

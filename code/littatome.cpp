4cccf438ed9a3c8358390e6837180335471ce5cf
Theo <theo.hordequin@gmail.com>
2016-06-01 19:58:20 +0200

    litteral atome raz

---
+++ code/littatome.cpp
@@ -0,0 +1,6 @@
+#include "littatome.h"
+
+littatome::littatome()
+{
+
+}
---
+++ code/littatome.h
@@ -0,0 +1,12 @@
+#ifndef LITTATOME_H
+#define LITTATOME_H
+#include <QString>
+
+class littatome
+{
+    QString identificateur;
+public:
+    littatome(QString i):identificateur(i);
+};
+
+#endif // LITTATOME_H

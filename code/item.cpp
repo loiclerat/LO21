f96f8364e938638d99b80e958b1e0e4428361efd
loiclerat <loic.lerat@gmail.com>
2016-06-04 17:11:32 +0200

    commentaires item

--- code/item.cpp
+++ code/item.cpp
@@ -1,6 +1,17 @@
+/**
+\file item.cpp
+\date 03/06/2016
+\author LoÃ¯c Lerat, AndrÃ©a Vibert, ThÃ©o Hordequin
+\version 1.0
+\brief  Définition des méthodes de la classe Item
+
+
+**/
+
 #include "item.h"
 #include "exceptions.h"
 
+//! \brief Retourne une référence vers la littérale associée à l'Item
 litterale& Item::getLitterale() const {
         if (lit==0) throw ComputerException("Item : tentative d'acces a une litterale inexistante");
         return *lit;
--- code/item.h
+++ code/item.h
@@ -1,14 +1,37 @@
+/**
+\file item.h
+\date 03/06/2016
+\author LoÃ¯c Lerat, AndrÃ©a Vibert, ThÃ©o Hordequin
+\version 1.0
+\brief  Elements de la Pile
+
+Description de la classe Item
+
+**/
+
 #ifndef ITEM_H
 #define ITEM_H
 
 #include "litterale.h"
 
+/**
+  \class Item
+  \brief La classe Item est chargée de "stocker" un littérale pour être ensuité empilé sur la Pile
+ */
+
 class Item {
+    //! \brief Littérale associée à l'Item
     litterale* lit;
+
 public:
+
+    //! \brief Constructeur
     Item():lit(0){}
+    //! \brief Associer une littérale à l'Item
     void setLitterale(litterale& l) { lit=&l; }
+    //! \brief Dissocier l'Item de la littérale associée
     void raz() { lit=0; }
+    //! \brief Retourne une référence vers la littérale associée à l'Item
     litterale& getLitterale() const;
 };
 

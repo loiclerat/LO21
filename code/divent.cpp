1f9f5f8c50f9857f8c766a980309217d74ec5e4c
Theo <theo.hordequin@gmail.com>
2016-06-04 22:05:29 +0200

    Ajout de l'operateur div entiere

--- code/divent.cpp
+++ code/divent.cpp
@@ -1,27 +1,24 @@
 #include "divent.h"
-/*
 
 litterale& divent::traitement(litterale &a, litterale &b){
 
-
     littEntiere* ent1 = dynamic_cast<littEntiere*>(&a);
-    littEntiere* ent2 = dynamic_cast<littEntiere*>(&b);
+    littNumerique* ent2 = dynamic_cast<littNumerique*>(&b);
 
-    if(ent1 != nullptr){
-        if(ent2!=nullptr){
-            littReelle* res =  *(ent1)/ent2;
-            return *res;
-        }else {
-            throw ComputerException("Format incompatible du premier argument, nécessite entier");
-        }
-    }else{
+    if(ent1 != nullptr && ent2!=nullptr)
+    {
+            littNumerique* res =  (*(ent1)/ent2);
+            littrat* rat1 = dynamic_cast<littrat*>(res);
+                if(rat1 != nullptr){
+                    int a = rat1->getNum()/rat1->getDen();
+                    littEntiere* resf = new littEntiere(a);
+                    return *resf;
+                }
+                else{
                     throw ComputerException("Format incompatible du second argument, nécessite entier");
                 }
+        }else {
+            throw ComputerException("Format incompatible du second argument, nécessite entier");
 
+        }
 }
-
-
-litterale& divent::traitement(litterale& a){
-
-}
-*/
--- code/divent.h
+++ code/divent.h
@@ -7,15 +7,15 @@
 #include "littrat.h"
 #include "littentiere.h"
 #include "littreelle.h"
+#include "opebinaire.h"
 
 
-class divent : public operateur_numerique
+class divent : public operateur_numerique, public binaire
 {
     QString symbol;
 public:
     divent(QString s):symbol(s),operateur_numerique(2){}
     litterale& traitement(litterale &a, litterale &b);
-    litterale& traitement(litterale &a);
 };
 
 

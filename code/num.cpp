bbf9db46820e71efbd9d6fcc72c3c0ec5423c9ad
Andrea Vibert <andrea.vibert@etu.utc.fr>
2016-06-06 23:09:49 +0200

    Modification de l'opérateur NUM pour qu'il fonctionne sur entiers et qu'il crée une littérale

--- code/num.cpp
+++ code/num.cpp
@@ -5,14 +5,19 @@
 litterale& num::traitement(litteraleManager& mng, litterale &a){
 
     littrat* b = dynamic_cast<littrat*>(&a);
+    littEntiere *c=dynamic_cast<littEntiere*>(&a);
 
     if(b != 0)
     {
-        littEntiere t(b->getNum());
-        return mng.addLitterale(&t);
-        }else {
-            throw ComputerException("Ce n'est pas une litterale rationnelle");
-
+        littEntiere* t =  new littEntiere(b->getNum());
+        return mng.addLitterale(t);
+    }
+    else{
+        if(c!=0){
+            littEntiere* t =  new littEntiere(c->getValeur());
+            return mng.addLitterale(t);
         }
+        else throw ComputerException("On ne peut pas retourner le numérateur de cette littérale.");
+    }
 }
 
